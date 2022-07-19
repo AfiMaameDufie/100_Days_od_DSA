c503e4aae3fd2e0df1ec579d9985485afbaed0fc
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-19 09:12:56 +0000

    FizzBuzz Question

---
+++ basics.py
@@ -0,0 +1,42 @@
+"""
+    Enumerate vs Range ()
+    >> ipython --no-banner -i $file.py
+    >>call function and pass args
+    >>exit() shell
+
+    Sample Easy Question
+
+"""
+
+def fizz_buzz(numbers):
+    """
+        Given a list of integers, 
+        1. Replace all integers divisible by 3 with "fizz"
+        2. Replace all integers divisible by 5 with "buzz"
+        3. Replace all integers divisible by both 3 and 5 with "fizzbuzz"
+
+        >>> numbers = [45, 22, 14, 65, 97, 72]
+        >>> fizz_buzz(numbers)
+        >>> numbers
+        ['fizzbuzz', 22, 14, 'buzz', 97, 'fizz']
+    """
+
+    # Using range()
+
+    for i in range(len(numbers)):
+        if numbers[i] % 3 == 0 :
+            numbers[i] = "fizz"
+        if numbers[i] % 5 == 0 :
+            numbers[i] = "buzz"
+        if numbers[i] % 3 == 0  and numbers[i] % 5 == 0:
+            numbers[i] = "fizzbuzz"
+
+
+    for i in range(len(numbers)):
+        num = numbers[i]
+        if num % 3 == 0 :
+            numbers[i] = "fizz"
+        if num % 5 == 0 :
+            numbers[i] = "buzz"
+        if num % 3 == 0  and num % 5 == 0:
+            numbers[i] = "fizzbuzz"
