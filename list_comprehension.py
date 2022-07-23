79ee59d4ec6d8f6a5f4935ca7dce62584ed65213
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-23 22:41:20 +0000

    Grid question

--- list_comprehension.py
+++ list_comprehension.py
@@ -41,3 +41,53 @@
 # >>>
 # >>> list(filter(is_odd, lst))
 # [1, -5, 9]
+
+
+# >>> [is_odd(num) for num in lst]
+# [True, False, True, False, True, False]
+
+
+
+# Grid Question
+grid = [[0,0,0],
+        [0,0,0]]
+
+num_rows = 2
+num_cols = 3
+
+for _ in range(num_rows):
+     curr_row = []
+     for _ in range(num_cols):
+             curr_row.append(0)
+     grid.append(curr_row)
+
+
+grid
+[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+grid
+[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+
+
+
+# Finally, haha
+# afimaamedufie@Afis-MacBook-Pro 100_Days_od_DSA % python3
+# Python 3.10.2 (main, May 10 2022, 11:34:48) [Clang 13.1.6 (clang-1316.0.21.2.3)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> num_rows = 2
+# >>>
+# >>> num_cols = 3
+# >>>
+# >>> grid = []
+# >>>
+# >>> for _ in range(num_rows):
+# ...     curr_row = []
+# ...     for _ in range(num_cols):
+# ...             curr_row.append(0)
+# ...     grid.append(curr_row)
+# ...
+# >>>
+# >>> grid
+# [[0, 0, 0], [0, 0, 0]]
+# >>>
