f7b2f045854a3aa6467e2842205e9872ba81688e
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-24 22:03:41 +0000

    Exploring sorting

---
+++ sorting.py
@@ -0,0 +1,9 @@
+animals = ['lion', 'cheeetah', 'rhino', 'dog', 'ape']
+sorted(animals)
+
+
+# Output
+# >>> animals = ['lion', 'cheeetah', 'rhino', 'dog', 'ape']
+# >>> sorted(animals)
+# ['ape', 'cheeetah', 'dog', 'lion', 'rhino']
+
