dd8edfd239b99fad05114ce2795966375d5f6409
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-24 21:49:01 +0000

    Fstrings with func example

--- fstrings.py
+++ fstrings.py
@@ -19,3 +19,69 @@ print(f"My name is {name}. I am {age + 5} years old.")
 # My name is Afi. I am 10 years old.
 # >>> print(f"My name is {name}. I am {age + 5} years old.")
 # My name is Afi. I am 15 years old.
+
+class A(object):
+    age = 10
+    name = "Afi"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __repr__(self):
+        return f"""
+            My name is {self.name}.
+            I am {self.age + 14} years old.
+        """
+print(A(name, age))
+
+#  Output
+# >>> class A(object):
+# ...     age = 10
+# ...     name = "Afi"
+# ...     def __init__(self, name, age):
+# ...         self.name = name
+# ...         self.age = age
+# ...     def __repr__(self):
+# ...         return f"""
+# ...             My name is {self.name}.
+# ...             I am {self.age + 14} years old.
+# ...         """
+# ... 
+# >>> print(A(name, age))
+
+#             My name is Afi.
+#             I am 24 years old.
+
+
+class A(object):
+    age = 10
+    name = "Afi"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __repr__(self):
+        return (
+                f"My name is {self.name}."
+                f"I am {self.age + 14} years old."
+                )
+print(A(name, age))
+
+
+
+# Output
+
+# >>> class A(object):
+# ...     age = 10
+# ...     name = "Afi"
+# ...     def __init__(self, name, age):
+# ...         self.name = name
+# ...         self.age = age
+# ...     def __repr__(self):
+# ...         return (
+# ...                 f"My name is {self.name}."
+# ...                 f"I am {self.age + 14} years old."
+# ...                 )
+# ... 
+# >>> print(A(name, age))
+# My name is Afi.I am 24 years old.
