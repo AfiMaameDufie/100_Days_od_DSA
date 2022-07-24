00bf087bb5f997c12b2e2866549b2489b20ca611
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-24 22:18:56 +0000

    Sorting with dicts

--- sorting.py
+++ sorting.py
@@ -1,5 +1,7 @@
 animals = ['lion', 'cheeetah', 'rhino', 'dog', 'ape']
 sorted(animals)
+# To reverse output
+sorted(animals, reverse=True)
 
 
 # Output
@@ -7,3 +9,27 @@ sorted(animals)
 # >>> sorted(animals)
 # ['ape', 'cheeetah', 'dog', 'lion', 'rhino']
 
+# Something a bit more complex
+# shift, option, arrow 
+animals = [
+    {'type': 'cat', 'name': 'Stephanie', 'age': '5'},
+    {'type': 'rhino', 'name': 'Moe', 'age': '7'}, 
+    {'type': 'dog', 'name': 'Billie', 'age': '3'}
+]
+sorted(animals, key=lambda animal: animal['age'])
+sorted(animals, key=lambda animal: animal['age'], reverse=True)
+sorted(animals, key=lambda animal: animal['age'], reverse=True)[0]
+
+# Output
+# >>> animals = [
+# ...     {'type': 'cat', 'name': 'Stephanie', 'age': '5'},
+# ...     {'type': 'rhino', 'name': 'Moe', 'age': '7'}, 
+# ...     {'type': 'dog', 'name': 'Billie', 'age': '3'}
+# ... ]
+# >>> 
+# >>> sorted(animals, key=lambda animal: animal['age'])
+# [{'type': 'dog', 'name': 'Billie', 'age': '3'}, {'type': 'cat', 'name': 'Stephanie', 'age': '5'}, {'type': 'rhino', 'name': 'Moe', 'age': '7'}]
+# >>> sorted(animals, key=lambda animal: animal['age'], reverse=True)
+# [{'type': 'rhino', 'name': 'Moe', 'age': '7'}, {'type': 'cat', 'name': 'Stephanie', 'age': '5'}, {'type': 'dog', 'name': 'Billie', 'age': '3'}]
+# >>> sorted(animals, key=lambda animal: animal['age'], reverse=True)[0]
+# {'type': 'rhino', 'name': 'Moe', 'age': '7'}
