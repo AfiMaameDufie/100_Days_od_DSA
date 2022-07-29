a82d251d6da0a04b44c49e6c66dfc0bf71226bd3
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-29 22:39:10 +0000

    Neatening up

--- num_of_steps_to_zero.py
+++ num_of_steps_to_zero.py
@@ -24,3 +24,18 @@ class Solution:
                 counter=counter + 1
 
             return counter
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        
+            counter = 0 
+            while num!= 0:
+                if (num % 2 == 0):
+                    num = num // 2
+
+                else:
+                    num-=1
+                    
+                counter+=1
+
+            return counter
