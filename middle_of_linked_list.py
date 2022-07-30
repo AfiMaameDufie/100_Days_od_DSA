c16984fbec2b48842b3b892c1b7466301effcc70
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-30 17:14:42 +0000

    Finding middle of the linked list

---
+++ middle_of_linked_list.py
@@ -0,0 +1,17 @@
+'''
+    876. Middle of the Linked List
+    Given the head of a singly linked list, return the middle node of the linked list.
+
+    If there are two middle nodes, return the second middle node.
+
+    Input: head = [1,2,3,4,5]
+    Output: [3,4,5]
+    Explanation: The middle node of the list is node 3.
+
+    Constraints:
+
+    The number of nodes in the list is in the range [1, 100].
+    1 <= Node.val <= 100
+
+'''
+
