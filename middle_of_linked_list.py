884f776762398ae1bccb7ba5aabe598d1944af76
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-30 17:15:55 +0000

    Finding middle of the linked list

--- middle_of_linked_list.py
+++ middle_of_linked_list.py
@@ -15,3 +15,18 @@
 
 '''
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Floyd's Tortoise and Hare algorithm
+    
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next :
+            slow = slow.next
+            fast = fast.next.next
+        return slow
