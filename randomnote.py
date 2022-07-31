380d1eb429a16597987b1528e2ac87d0a418d1e6
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-07-31 22:34:42 +0000

    Ransom Note Solution using HashMap: Note and typo corrections

--- randomnote.py
+++ randomnote.py
@@ -30,14 +30,26 @@
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hash_random = collections.Counter(ransomNote)
+        hash_ransom = collections.Counter(ransomNote)
         hash_magazine = collections.Counter(magazine)
         
         
-        for i in hash_random:
+        for i in hash_ransom:
             if i in hash_magazine:
-                if i in hash_magazine[i] > hash_random[i]:
+                if i in hash_magazine[i] > hash_ransom[i]:
                     return False
             else:
                 return False
         return True
+
+
+'''
+    "collections.counter()" counts every charcter in the string and return a dictonary. key as character and value as count example- "aabbcddd" >>{"a":2,"b":2,"c":1,"d":3}
+    in the first for loop, we get the keys of the the ransom note into i to get counts of ransom and magazine
+    then the next if checks if char in hash ransom is in hash magazine
+    then the next if checks the count because in the question it stated that the count for num of char in ransom note is less than that in magazine
+    So it returns a false or exits the loop and comtinues
+    if the char in hash ransom is not in hash maagzine, return false
+    if no conditions are met, return false
+    
+'''
