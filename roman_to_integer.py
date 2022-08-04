3dfbeb8d09a9b5005b074fb57987bf1eaeaaa6b5
abigail.gbadago <abigail.gbadago@mpharma.com>
2022-08-04 21:21:59 +0000

    Roman to Integer Solution; first two solutions rejected

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -43,3 +43,16 @@
     It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
 '''
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        
+        val = 0
+
+        for k, v in map.items():
+            if k in s:
+                val += s.count(k) * v
+                s = s.replace(k, "")
+
+        return val
