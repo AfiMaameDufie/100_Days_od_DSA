9224f7866d1128a70388083acb51478591622d35
abigail.gbadago <afimaamedufie@gmail.com>
2022-08-18 13:54:30 +0000

    Merge Sorted Arrays Alternative Solution

--- merge_sorted_arrays.py
+++ merge_sorted_arrays.py
@@ -45,3 +45,15 @@ class Solution:
         nums1[m:] = nums2
         
         nums1.sort()
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+#         nums1[m:] = nums2
+        
+#         nums1.sort()
+
+        nums1[:] = sorted(nums1[:m]+nums2)
