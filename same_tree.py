8f963724efc2e3c425d46d380000fca1de6d0e9a
abigail.gbadago <afimaamedufie@gmail.com>
2022-08-24 00:50:00 +0000

    Same Tree Recursive Solution

--- same_tree.py
+++ same_tree.py
@@ -16,3 +16,29 @@
     Input: p = [1,2], q = [1,null,2]
     Output: false
 '''
+
+# Recursion
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+#         Checking to see if p and q are both None
+        if not p and not q:
+            return True
+#         one of p and q is None
+        if not q or not p:
+            return False
+        
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+    
+
+# Algorithm Complexity
+# Time : O(N)
+# Space : O(N)
