3b144c6cd1f38a057d4c64bacdb6201245ba5a1e
abigail.gbadago <afimaamedufie@gmail.com>
2022-08-25 19:42:15 +0000

    Is Symmetrical Solution does not work

--- is_symmetric.py
+++ is_symmetric.py
@@ -20,3 +20,26 @@
     -100 <= Node.val <= 100
 
 '''
+
+class Solution():
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:  
+        if not root:
+            return True;
+                # Return the function recursively...
+        return self.isSimilar(root.left,root.right)
+    
+#         For symmetric trees, the left subtree reflection is a mirror of the right subtree
+    def isSimilar(self, leftroot, rightroot):
+#             If both nodes are null/ nodes are null pointers, return True
+        if leftroot and rightroot == None:
+            return True
+#        If exactly one of them is a null node, return False
+        if leftroot or rightroot == None:
+            return False
+#         If root nodes do not have the same value, return False
+        if leftroot.val != rightroot.val:
+            return False
+        
+#       Return true if the values of root nodes are same and left as well as right subtrees are symmetric / on both sides
+        return self.isSimilar(leftroot.left,rightroot.right) and self.isSimilar(leftroot.right,rightroot.left)
+
