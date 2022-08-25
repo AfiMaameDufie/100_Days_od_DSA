8d3f2e9adfbad2ffac718bfa56fad14c1adb2a7c
abigail.gbadago <afimaamedufie@gmail.com>
2022-08-25 19:47:32 +0000

    Is Symmetrical Solution DFS

--- is_symmetric.py
+++ is_symmetric.py
@@ -43,3 +43,26 @@ class Solution():
 #       Return true if the values of root nodes are same and left as well as right subtrees are symmetric / on both sides
         return self.isSimilar(leftroot.left,rightroot.right) and self.isSimilar(leftroot.right,rightroot.left)
 
+
+#  Working solution DFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution():
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:    
+        if not root:
+            return True
+         # Return the function recursively...
+        return self.dfs(root.left, root.right)
+
+    def dfs(self, l, r):
+        if l and r:
+    # Return true if the values of root nodes are same and left as well as right subtrees are symmetric / on both sides
+            return l.val == r.val and self.dfs(l.left, r.right) and self.dfs(l.right, r.left)
+        return l == r
+
+
